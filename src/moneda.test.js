c496e9f5e6f66baa6cb3f5d3c06c2a6edeea4f00
susan-cespedes <susan.cespedes@ucb.edu.bo>
2023-12-13 18:04:52 -0400

    devolver un vector con 1

--- src/moneda.test.js
+++ src/moneda.test.js
@@ -4,4 +4,8 @@ describe ("cambio monedas", ()=>{
     it ("devolver un vector vacio si el monto ingreado es 0", ()=>{
         expect (cambioMonedas (0)).toEqual([])
     })
+
+    it ("devolver un vector con 1", ()=>{
+        expect (cambioMonedas (1)).toEqual([1])
+    })
 })
