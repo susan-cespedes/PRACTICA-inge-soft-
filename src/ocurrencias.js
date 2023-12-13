a555fa02e753fd9a8c8e95a2c33099a091a34bf8
susan-cespedes <susan.cespedes@ucb.edu.bo>
2023-12-13 18:49:25 -0400

    contar la cantidad de ocurrencias de las palabras con una ocurrencia

--- src/ocurrencias.js
+++ src/ocurrencias.js
@@ -2,7 +2,16 @@
 function contarOcurrencias(cadena){
     let resultado ={}
     if (cadena.length > 0){
-        resultado [cadena]=1;
+    let listaPalabras = cadena.split(" ");
+    listaPalabras.forEach (palabra=>{
+        if (resultado [palabra]){
+            resultado [palabra]+=1
+        }
+        else{
+            resultado [palabra]=1;
+        }
+    })
+        
     }
     return resultado;
 }
